39a422b396674fd448f670dc9e4987a3b532d299
seeeni <seyeon0207@gmail.com>
2023-04-16 21:17:08 +0900

    BOJ code 0416 version2

--- 02. Silver/13015.py
+++ 02. Silver/13015.py
@@ -4,45 +4,45 @@ n = int(sys.stdin.readline().strip())
 for i in range(n):
     for j in range(2 * n - 1):
         if i == 0 and j < n:
-            print("*", end = "")
+            sys.stdout.write("*")
             continue
 
         if j == i or j == n + i -1:
-            print("*", end = "")
+            sys.stdout.write("*")
         else:
-            print(" ", end = "")
+            sys.stdout.write(" ")
     for j in range(2 * n - 3, -1, -1):
         if i == 0 and j < n:
-            print("*", end = "")
+            sys.stdout.write("*")
             continue
 
         if j == i or j == n + i -1:
-            print("*", end = "")
+            sys.stdout.write("*")
         else:
-            print(" ", end = "")
+            sys.stdout.write(" ")
         if j == i:
             break
-    print()
+    sys.stdout.write("\n")
 
 for i in range(n-2, -1, -1):
     for j in range(2 * n - 1):
         if i == 0 and j < n:
-            print("*", end = "")
+            sys.stdout.write("*")
             continue
 
         if j == i or j == n + i -1:
-            print("*", end = "")
+            sys.stdout.write("*")
         else:
-            print(" ", end = "")
+            sys.stdout.write(" ")
     for j in range(2 * n - 3, -1, -1):
         if i == 0 and j < n:
-            print("*", end = "")
+            sys.stdout.write("*")
             continue
 
         if j == i or j == n + i -1:
-            print("*", end = "")
+            sys.stdout.write("*")
         else:
-            print(" ", end = "")
+            sys.stdout.write(" ")
         if j == i:
             break
-    print()
+    sys.stdout.write("\n")
